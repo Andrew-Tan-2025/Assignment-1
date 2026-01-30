fc5d48813ea942bdc4137638bcf3a7ab571f001b
Andrew-Tan-622 <95309123+Andrew-Tan-2025@users.noreply.github.com>
2026-01-29 20:43:41 -0700

    Allowed downloading from link

--- assignment1.sh
+++ assignment1.sh
@@ -10,16 +10,13 @@ sudo apt install unzip
 
 # DOWNLOAD ALL CSV FILES TO ROOT OF ASSIGNMENT
 # authors.csv
-#curl -L -o authors.csv "https://www.dropbox.com/scl/fo/espd9jgl8owm9j6jg3ymo/AO5i_y7fzUxmSnQfu0b_0fQ/authors.csv?rlkey=y0hfra01ux0bg6i2xue6ytka1&dl=0"
-
+curl -L -o authors.csv "https://www.dropbox.com/scl/fo/espd9jgl8owm9j6jg3ymo/AO5i_y7fzUxmSnQfu0b_0fQ/authors.csv?rlkey=y0hfra01ux0bg6i2xue6ytka1&dl=0"
 # comments.csv
-#curl -L -o comments.csv "https://www.dropbox.com/scl/fo/espd9jgl8owm9j6jg3ymo/AO9m6AiVoPJcawa7uxXYh5Q/comments.csv?rlkey=y0hfra01ux0bg6i2xue6ytka1&dl=0"
-
+curl -L -o comments.csv "https://www.dropbox.com/scl/fo/espd9jgl8owm9j6jg3ymo/AO9m6AiVoPJcawa7uxXYh5Q/comments.csv?rlkey=y0hfra01ux0bg6i2xue6ytka1&dl=0"
 # submissions.csv
-#curl -L -o submissions.csv "https://www.dropbox.com/scl/fo/espd9jgl8owm9j6jg3ymo/AOe-Ju_iwWeZ7J4HiNqk7QY/submissions.csv?rlkey=y0hfra01ux0bg6i2xue6ytka1&dl=0"
-
+curl -L -o submissions.csv "https://www.dropbox.com/scl/fo/espd9jgl8owm9j6jg3ymo/AOe-Ju_iwWeZ7J4HiNqk7QY/submissions.csv?rlkey=y0hfra01ux0bg6i2xue6ytka1&dl=0"
 # subreddits.csv
-#curl -L -o subreddits.csv "https://www.dropbox.com/scl/fo/espd9jgl8owm9j6jg3ymo/AJJtP1wpnqc7gcgh4Yaklwk/subreddits.csv?rlkey=y0hfra01ux0bg6i2xue6ytka1&dl=0"
+curl -L -o subreddits.csv "https://www.dropbox.com/scl/fo/espd9jgl8owm9j6jg3ymo/AJJtP1wpnqc7gcgh4Yaklwk/subreddits.csv?rlkey=y0hfra01ux0bg6i2xue6ytka1&dl=0"
 
 
 psql -h 127.0.0.1 -p 5432 -U postgres -d postgres -f schema.sql
